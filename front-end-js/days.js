4f07e467cc5608d931d3f6826dabde2a5d7dce70
Ronda Cowen <cowenhome5@gmail.com>
2021-01-26 19:15:38 -0600

    front end push

--- front-end-js/days.js
+++ front-end-js/days.js
@@ -1,13 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => loadFasts())
+document.addEventListener("DOMContentLoaded", () => loadDays())
 
-const renderFast = (fastHash) => {
-
+const renderDay = (dayHash) => {
     const div = document.createElement("div")
     const ul = document.createElement("ul")
     const li = document.createElement("li")
 
-    li.innerHTML = fastHash.start
-    li.innerHTML = fastHash.end
+    li.innerHTML = dayHash.name_of_day
+    li.innerHTML = dayHash.total_hours_fasted
     ul.appendChild(li)
     div.appendChild(ul)
 
